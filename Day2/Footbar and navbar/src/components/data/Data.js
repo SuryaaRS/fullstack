0f5517a63cc9373ca0119ff8a968a9dd02244ee6
SuryaaRS <120247114+SuryaaRS@users.noreply.github.com>
2024-03-19 10:45:46 +0530

    Add files via upload

---
+++ Day2/Footbar and navbar/src/components/data/Data.js
@@ -0,0 +1,29 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" },  { list: "Contact Page" }],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [{ list: "Headers" },  { list: "Footers" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }],
+  },
+]
---
+++ Day2/Footbar and navbar/src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+// import Awards from "./awards/Awards"
+import Hero from "./hero/Hero"
+// import Price from "./price/Price"
+
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      {/* <Awards /> */}
+      {/* <Price /> */}
+    </>
+  )
+}
+
+export default Home
